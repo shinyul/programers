335de594b1eb55704d712f2a74dd186710253832
shin <sindepal@gmail.com>
2019-12-26 17:28:53 +0900

    modify #23 리뷰 사항 수정 완료

--- src/level_1/greedy/gym_cloth/sanghoon.java
+++ src/level_1/greedy/gym_cloth/sanghoon.java
@@ -1,11 +1,11 @@
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int answer = n-lost.length;
-        
-        for ( int i=0; i < reserve.length; i++ ) {
-        	for(int k=0; k < lost.length; k++ ) {
+        int answer = n - lost.length;
+
+        for ( int i = 0; i < reserve.length; i++ ) {
+        	for ( int k=0; k < lost.length; k++ ) {
         		if ( reserve[i] == lost[k] ) {
-                	lost[k] = -1; 
+                    lost[k] = -1; 
                     reserve[i] = -1;
                     answer++;   
                     break;
@@ -13,19 +13,18 @@ class Solution {
         	}
         }
         
-        for ( int i=0; i < reserve.length; i++ ) {
-        	if(reserve[i] > -1 ) {
-        		for (int j=0; j < lost.length; j++ ) {        			
+        for ( int i = 0; i < reserve.length; i++ ) {
+        	if ( reserve[i] > -1 ) {
+        		for ( int j = 0; j < lost.length; j++ ) {        			
             		if ( lost[j] > -1 
-            				&& ( (reserve[i]+1) == lost[j] ||  (reserve[i]-1) == lost[j] ) ) {
-            			lost[j] = -1; 
-            			reserve[i] = -1;
-            			answer++;   
-            			break;
+            				&& ( ( reserve[i] + 1 ) == lost[j] ||  ( reserve[i] - 1 ) == lost[j] ) ) {
+                        lost[j] = -1; 
+                        reserve[i] = -1;
+                        answer++;   
+                        break;
             		}
             	}	
         	}
-        	
         } 
     	return answer;
     }
