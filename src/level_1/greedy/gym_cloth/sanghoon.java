9286193871dcc748098def920a2bcf335ba9a1dd
shin <sindepal@gmail.com>
2019-12-26 17:38:26 +0900

    modfy #23 space 4 변경

--- src/level_1/greedy/gym_cloth/sanghoon.java
+++ src/level_1/greedy/gym_cloth/sanghoon.java
@@ -3,8 +3,8 @@ class Solution {
         int answer = n - lost.length;
 
         for ( int i = 0; i < reserve.length; i++ ) {
-        	for ( int k=0; k < lost.length; k++ ) {
-        		if ( reserve[i] == lost[k] ) {
+            for ( int k=0; k < lost.length; k++ ) {
+                if ( reserve[i] == lost[k] ) {
                     lost[k] = -1; 
                     reserve[i] = -1;
                     answer++;   
@@ -15,17 +15,18 @@ class Solution {
         
         for ( int i = 0; i < reserve.length; i++ ) {
         	if ( reserve[i] > -1 ) {
-        		for ( int j = 0; j < lost.length; j++ ) {        			
-            		if ( lost[j] > -1 
-            				&& ( ( reserve[i] + 1 ) == lost[j] ||  ( reserve[i] - 1 ) == lost[j] ) ) {
+                for ( int j = 0; j < lost.length; j++ ) {        			
+                    if ( lost[j] > -1 
+                        && ( ( reserve[i] + 1 ) == lost[j] ||  ( reserve[i] - 1 ) == lost[j] ) ) {
                         lost[j] = -1; 
                         reserve[i] = -1;
                         answer++;   
                         break;
-            		}
-            	}	
-        	}
+                    }
+                }
+            }
         } 
-    	return answer;
+        return answer;
     }
 }
+
